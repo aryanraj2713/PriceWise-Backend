e105eafc2fa4576327ee7b106340f56cf55f390d
Anubhav Pathak <anubhavpathak99@gmail.com>
2023-04-14 10:41:43 +0530

    :package: :bug: Fixed File Uploads

--- app.py
+++ app.py
@@ -3,41 +3,53 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 from flask import Flask, jsonify, request
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.secret_key = "shit-secret-key"
 
-model = tf.keras.models.load_model('weights.h5')
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+model = tf.keras.models.load_model("weights.h5")
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    if 'file' not in request.files:
-        return jsonify({'error': 'no file uploaded'})
     
-    file = request.files['file']
+    if 'files[]' not in request.files:
+        resp = jsonify({'message' : 'No file part in the request'})
+        resp.status_code = 400
+        return resp
+ 
+    files = request.files.getlist('files[]')
+     
+    errors = {}
+    results = []
+     
+    for file in files:      
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            img = image.load_img(os.path.join(app.config['UPLOAD_FOLDER'], filename), target_size=(125, 125))
+            x = image.img_to_array(img)
+            x = x / 255.0
+            x = np.expand_dims(x, axis=0)
+            prediction = model.predict(x)
+            if prediction < 0.5:
+                result = 'damaged'
+            else:
+                result = 'not damaged'
+            results.append(result)
+        else:
+            errors[file.filename] = 'File type is not allowed'
+            results.append(errors)
     
-    if not file or not allowed_file(file.filename):
-        return jsonify({'error': 'file type not allowed'})
-    
-    img = image.load_img(file, target_size=(125, 125))
-
-    x = image.img_to_array(img)
-
-    x = x / 255.0
-
-    x = np.expand_dims(x, axis=0)
-
-    prediction = model.predict(x)
-
-    if prediction < 0.5:
-        result = 'damaged'
-    else:
-        result = 'not damaged'
-    
-    return jsonify({'result': result})
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in {'png', 'jpg', 'jpeg'}
+    return jsonify(results)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
